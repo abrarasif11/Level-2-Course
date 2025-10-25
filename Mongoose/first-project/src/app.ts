27a6c25cd89fbf46f7967c5daad5e6da563c9653
abrarasif11 <faasif929@gmail.com>
2025-10-25 23:44:17 +0600

    Install prettierts

--- Mongoose/first-project/src/app.ts
+++ Mongoose/first-project/src/app.ts
@@ -1,15 +1,15 @@
-import express, {Application, Request, Response} from 'express'
-import cors from 'cors'
-const app: Application = express()
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
 
 //parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req: Request, res : Response) => {
-  var a = 10
-  res.send(a)
-})
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
 
 // console.log(process.cwd());
 
