77ef31b7a4fa5331d71f8df1b92c6ce616de59b3
abrarasif11 <faasif929@gmail.com>
2025-11-06 19:01:12 +0600

    ALTER Keyword for Table Modification

--- PostgreSQL/alter.sql
+++ PostgreSQL/alter.sql
@@ -1,7 +1,30 @@
 SELECT * FROM person;
+ 
 
+--  Add column 
 ALTER TABLE person
     ADD COLUMN email VARCHAR(25) DEFAULT 'default@gmail.com' NOT NULL;
 
 INSERT INTO person (first_name, last_name, age, is_active, dob, email)
 VALUES ('Tested', 'User', 25, TRUE, '1990-01-01', 'tested @gmail.com');
+
+-- delete column 
+ALTER TABLE person
+    DROP COLUMN email;
+
+    -- rename column
+ALTER Table person
+   RENAME COLUMN age to user_age;
+
+ALTER TABLE person
+ADD COLUMN user_age INTEGER;
+
+-- change type 
+ALTER TABLE person
+    ALTER COLUMN user_email TYPE VARCHAR(50);
+
+-- delete contain 
+ALTER TABLE person
+    ALTER COLUMN user_age DROP NOT NULL;
+
+
