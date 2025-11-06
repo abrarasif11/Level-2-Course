90d58cd82830d38444bcfb87de66bc8d77cf47b0
abrarasif11 <faasif929@gmail.com>
2025-11-06 22:44:43 +0600

    create new table

---
+++ PostgreSQL/select.sql
@@ -0,0 +1,22 @@
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age INT ,
+    grade VARCHAR(2),
+    course VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(50)
+);
+
+INSERT INTO students (first_name, last_name, age, grade, course, email, dob, blood_group, country)
+VALUES 
+('Fahim', 'Asif', 22, 'A', 'Computer Science', 'fahim.asif@example.com', '2003-05-12', 'A+', 'Bangladesh'),
+('Sara', 'Rahman', 21, 'B+', 'Electrical Engineering', 'sara.rahman@example.com', '2004-02-20', 'B+', 'Bangladesh'),
+('John', 'Doe', 23, 'A', 'Mechanical Engineering', 'john.doe@example.com', '2002-08-10', 'O+', 'USA'),
+('Mehedi', 'Hasan', 22, 'A-', 'Information Technology', 'mehedi.hasan@example.com', '2003-01-17', 'B-', 'Bangladesh'),
+('Anika', 'Sultana', 20, 'B', 'Business Administration', 'anika.sultana@example.com', '2005-03-25', 'AB+', 'India');
+
+SELECT * FROM students;
