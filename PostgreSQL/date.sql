c831f58be50958f2497a56eed765fe445b39fce3
abrarasif11 <faasif929@gmail.com>
2025-11-07 19:14:26 +0600

    show date and time

---
+++ PostgreSQL/date.sql
@@ -0,0 +1,14 @@
+
+-- show timezone;
+-- Date and Time function
+SELECT now();
+
+CREATE TABLE timeZ (ts TIMESTAMP Without time zone , tsz TIMESTAMP with time zone);
+
+INSERT INTO timez values('2025-11-07 10:30:00', '2025-11-07 10:30:00')
+
+SELECT * FROM timez;
+
+SELECT CURRENT_DATE;
+
+SELECT now()::time;
