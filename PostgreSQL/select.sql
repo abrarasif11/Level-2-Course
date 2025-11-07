6341d6fcfa334492de5e4483cd226ac8bc9afd30
abrarasif11 <faasif929@gmail.com>
2025-11-07 16:48:43 +0600

    Scalar and Aggregate Functions

--- PostgreSQL/select.sql
+++ PostgreSQL/select.sql
@@ -17,28 +17,65 @@ VALUES
 ('Sara', 'Rahman', 21, 'B+', 'Electrical Engineering', 'sara.rahman@example.com', '2004-02-20', 'B+', 'Bangladesh'),
 ('John', 'Doe', 23, 'A', 'Mechanical Engineering', 'john.doe@example.com', '2002-08-10', 'O+', 'USA'),
 ('Mehedi', 'Hasan', 22, 'A-', 'Information Technology', 'mehedi.hasan@example.com', '2003-01-17', 'B-', 'Bangladesh'),
-('Anika', 'Sultana', 20, 'B', 'Business Administration', 'anika.sultana@example.com', '2005-03-25', 'AB+', 'India');
+('Anika', 'Sultana', 20, 'B', 'Business Administration', 'anika.sultana@example.com', '2005-03-25', 'AB+', 'India'),
+('Rafi', 'Khan', 24, 'A+', 'Civil Engineering', 'rafi.khan@example.com', '2001-09-15', 'A+', 'Bangladesh'),
+('Nadia', 'Ahmed', 22, 'B+', 'Architecture', 'nadia.ahmed@example.com', '2003-06-28', 'O+', 'Bangladesh'),
+('Michael', 'Smith', 25, 'A', 'Computer Engineering', 'michael.smith@example.com', '2000-12-05', 'B+', 'Canada'),
+('Priya', 'Patel', 21, 'A-', 'Economics', 'priya.patel@example.com', '2004-01-30', 'AB-', 'India'),
+('Tarek', 'Hossain', 23, 'B', 'Software Engineering', 'tarek.hossain@example.com', '2002-04-14', 'A+', 'Bangladesh'),
+('Lamia', 'Akter', 20, 'A', 'Psychology', 'lamia.akter@example.com', '2005-11-09', 'O-', 'Bangladesh'),
+('David', 'Lee', 22, 'A', 'Data Science', 'david.lee@example.com', '2003-03-22', 'B+', 'USA'),
+('Muna', 'Chowdhury', 21, 'B+', 'Pharmacy', 'muna.chowdhury@example.com', '2004-07-18', 'AB+', 'Bangladesh'),
+('Omar', 'Faruk', 24, 'A-', 'Cyber Security', 'omar.faruk@example.com', '2001-10-03', 'O+', 'Bangladesh'),
+('Emily', 'Johnson', 23, 'B', 'Environmental Science', 'emily.johnson@example.com', '2002-09-12', 'A-', 'UK');
+
 
 -- use Order as Shorting 
--- SELECT * FROM students ORDER BY first_name ASC;
--- SELECT * FROM students ORDER BY age DESC;
+SELECT * FROM students ORDER BY first_name ASC;
+SELECT * FROM students ORDER BY age DESC;
 
 --  unique data
--- SELECT DISTINCT country from students;
--- SELECT DISTINCT blood_group from students;
+SELECT DISTINCT country from students;
+SELECT DISTINCT blood_group from students;
 
 -- data filtering 
 
--- SELECT * FROM students
---     WHERE country = 'Bangladesh';
-
--- SELECT * FROM students
---      WHERE grade = 'A' AND country = 'USA';
-
--- SELECT * FROM students
---     WHERE country = 'Bangladesh' OR country = 'USA' AND age='20';
-
--- data comparison
+SELECT * FROM students
+    WHERE country = 'Bangladesh';
 
 SELECT * FROM students
+     WHERE grade = 'A' AND country = 'USA';
+
+SELECT * FROM students
+    WHERE country = 'Bangladesh' OR country = 'USA' AND age='20';
+
+-- data comparison
+SELECT * FROM students
     WHERE country <> 'USA';
+
+-- Scaler and Aggregate function 
+SELECT * FROM students
+
+SELECT upper(first_name) as first_name_in_upper_case, * FROM students
+
+SELECT concat(first_name,' ',last_name) FROM students;
+
+SELECT length(first_name) FROM students;
+
+SELECT AVG(age) from students;
+
+SELECT MIN(age) from students;
+
+SELECT MAX(age) from students;
+
+SELECT SUM(age) from students;
+
+SELECT count(*) from students;
+
+
+SELECT max(length(first_name)) from students;
+
+
+
+
+
