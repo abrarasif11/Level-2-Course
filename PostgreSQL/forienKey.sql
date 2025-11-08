a6d5404920d95657a6fdf2e268dd9643fb3a688d
abrarasif11 <faasif929@gmail.com>
2025-11-08 10:56:06 +0600

    insert post data

--- PostgreSQL/forienKey.sql
+++ PostgreSQL/forienKey.sql
@@ -9,4 +9,38 @@ CREATE TABLE post(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     user_id INTEGER REFERENCES "user"(id)
-)
+)   
+
+
+INSERT INTO "user" (username) VALUES
+('akash'),
+('batash'),
+('shagor'),
+('nodi'),
+('priya');
+
+SELECT * FROM "user";
+
+TRUNCATE TABLE "user" RESTART IDENTITY CASCADE;
+
+INSERT INTO "user" (username) VALUES
+('akash'),
+('batash'),
+('shagor'),
+('nodi'),
+('priya');
+
+
+INSERT INTO post (title, user_id) VALUES
+INSERT INTO post (title, user_id) VALUES
+('My first blog post', 1),
+('Learning SQL basics', 2),
+('Exploring IoT projects', 3),
+('Travel diaries of Nodi', 4),
+('Priya’s event planning tips', 5);
+
+SELECT * FROM "user";
+
+
+
+
