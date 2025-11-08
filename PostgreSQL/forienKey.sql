372fa61882b44e6aae195d61e5131623669461f8
abrarasif11 <faasif929@gmail.com>
2025-11-08 10:37:17 +0600

    Foreign Key Constraints

--- PostgreSQL/forienKey.sql
+++ PostgreSQL/forienKey.sql
@@ -3,3 +3,10 @@ CREATE TABLE "user"(
     id SERIAL PRIMARY KEY,
     username VARCHAR(25) NOT NULL
 )
+
+-- Foreign Key Constraints
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    user_id INTEGER REFERENCES "user"(id)
+)
